63d274c3431cbbe01b541e2e6f5e763c753324b7
Matthew Baggett <matthew@baggett.me>
2014-03-05 23:27:29 +0000

    test connection to wallet

--- src/Models/Wallet.php
+++ src/Models/Wallet.php
@@ -3,10 +3,9 @@ namespace LoneSatoshi\Models;
 
 class Wallet{
   static public function call($command){
-    $configuration = new Ssh\Configuration(WALLET_ADDRESS);
-    $authentication = new Ssh\Authentication\Password(WALLET_USERNAME, WALLET_PASSWORD);
-
-    $session = new Ssh\Session($configuration, $authentication);
+    $configuration = new \Ssh\Configuration(WALLET_ADDRESS);
+    $authentication = new \Ssh\Authentication\Password(WALLET_USERNAME, WALLET_PASSWORD);
+    $session = new \Ssh\Session($configuration, $authentication);
     $exec = $session->getExec();
 
     return $exec->run("dogecoind {$command}", false, null, 80, 25, 0);
