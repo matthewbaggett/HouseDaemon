93213d1f629ee09424dbb49768b40f5118a1caaf
Matthew Baggett <matthew@baggett.me>
2014-03-06 11:18:01 +0000

    Push block count into a setting.

--- src/controllers/cron.php
+++ src/controllers/cron.php
@@ -3,6 +3,7 @@
 $app->get('/cron', function () use ($app) {
   \LoneSatoshi\Models\Wallet::update_transaction_log();
   $block_count = \LoneSatoshi\Models\Wallet::get_info('blocks');
+  echo "Block count is {$block_count}";
   \LoneSatoshi\Models\Setting::set("block_count", $block_count);
   die("Cron completed");
 });
