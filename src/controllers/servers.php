cd5140673930966de84bd32a5feee00f2eddbf48
Matthew Baggett <matthew@baggett.me>
2014-03-06 08:40:01 +0000

    Delete old controllers from inherited code

--- src/controllers/servers.php
+++
@@ -1,6 +0,0 @@
-<?php
-$app->map('/servers/list', function () use ($app) {
-  $response = new response();
-  $response->servers = server_model::search()->where('deleted', 'no')->where('private', 'no')->exec();
-  $response->json();
-})->via('GET', 'POST');
