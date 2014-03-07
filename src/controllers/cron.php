444d8eba7b7cc8f30f5601f6cfb248ed6455e5da
Matthew Baggett <matthew@baggett.me>
2014-03-07 13:54:19 +0000

    Add safe-guard to running cron

--- src/controllers/cron.php
+++ src/controllers/cron.php
@@ -1,9 +1,15 @@
 <?php
 
 $app->get('/cron', function () use ($app) {
-  \LoneSatoshi\Models\Wallet::update_transaction_log();
-  $block_count = \LoneSatoshi\Models\Wallet::get_info('blocks');
-  echo "Block count is {$block_count}";
-  \LoneSatoshi\Models\Setting::set("block_count", $block_count);
-  die("Cron completed");
+  $cron_last_run = \LoneSatoshi\Models\Setting::get("cron_last_run");
+  if(time() - 30 <= $cron_last_run){
+    \LoneSatoshi\Models\Wallet::update_transaction_log();
+    $block_count = \LoneSatoshi\Models\Wallet::get_info('blocks');
+    echo "Block count is {$block_count}";
+    \LoneSatoshi\Models\Setting::set("block_count", $block_count);
+    \LoneSatoshi\Models\Setting::set("cron_last_run", time());
+    die("Cron completed");
+  }else{
+    die("Too soon to run cron again");
+  }
 });
