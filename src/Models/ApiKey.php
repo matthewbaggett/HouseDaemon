a320f1326eaf5419dea4e73d57a155272bb37e82
Matthew Baggett <matthew@baggett.me>
2014-03-11 23:58:10 +0000

    Begining to write API

--- src/Models/ApiKey.php
+++ src/Models/ApiKey.php
@@ -13,14 +13,26 @@ class ApiKey extends \FourOneOne\ActiveRecord\ActiveRecord{
   public $revoked = 'No';
 
   /**
+   * @return ApiSession|false
+   */
+  public function get_current_session(){
+    return ApiSession::search()
+      ->where('api_key_id', $this->api_key_id)
+      ->where('expires', date("Y-m-d H:i:s"), ">=")
+      ->execOne();
+  }
+  /**
    * @return ApiSession
    */
   public function create_session(){
-    $session = new ApiSession();
-    $session->api_key_id = $this->api_key_id;
-    $session->created = date("Y-m-d H:i:s");
+    $session = $this->get_current_session();
+    if(!$session instanceof ApiSession){
+      $session = new ApiSession();
+      $session->api_key_id = $this->api_key_id;
+      $session->created = date("Y-m-d H:i:s");
+      $session->session_key = uniqid("Session_", true);
+    }
     $session->expires = date("Y-m-d H:i:s", time() + 1800);
-    $session->session_key = uniqid("Session_", true);
     $session->save();
     return $session;
   }
