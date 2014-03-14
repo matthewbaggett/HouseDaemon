a686c1d6728da9cb28cd9456a7c008017e27540c
Matthew Baggett <matthew@baggett.me>
2014-03-14 00:09:43 +0000

    Wrong path.

--- src/Models/Location.php
+++ src/Models/Location.php
@@ -23,7 +23,7 @@ class Location extends \FourOneOne\ActiveRecord\ActiveRecord{
       return $location;
     }else{
       $location = new Location();
-      $gi = geoip_open(APP_DISK_ROOT . "/GeoIP.dat", GEOIP_STANDARD);
+      $gi = geoip_open(APP_DISK_ROOT . "/geo/GeoIP.dat", GEOIP_STANDARD);
       $location->country = geoip_country_name_by_addr($gi, $ip_addr);
       $location->country_3 = geoip_country_code3_by_name($gi, $ip_addr);
       $location->continent = geoip_continent_code_by_name($gi, $ip_addr);
