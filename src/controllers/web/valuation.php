7dddd4f66d03835d01923805581f9cbeacdc9cef
Matthew Baggett <matthew@baggett.me>
2014-03-28 15:34:01 +0000

    Valuations support

--- src/controllers/web/valuation.php
+++ src/controllers/web/valuation.php
@@ -22,18 +22,10 @@ $app->get('/valuation/:coin/', function ($coina) use ($app) {
 
   $values = array();
   foreach(array('BTC', 'GBP', 'USD') as $coinb){
-    $values[$coinb] = \LoneSatoshi\Models\Valuation::search()
-      ->where('from', $coina)
-      ->where('to', $coinb)
-      ->where('source','average')
+    $values[$coinb] = \LoneSatoshi\Models\ExchangeRate::search()
+      ->where('input', $coina)
+      ->where('output', $coinb)
       ->exec();
-    if(!$values[$coinb]){
-      $values[$coinb] = \LoneSatoshi\Models\Valuation::search()
-        ->where('to', $coina)
-        ->where('from', $coinb)
-        ->where('source','average')
-        ->exec();
-    }
   }
   $app->render('valuation/track.phtml', array(
     'values' => $values,
