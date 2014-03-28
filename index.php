6d77f29a00b5700139d1d6af75a33eb44e5a5042
Matthew Baggett <matthew@baggett.me>
2014-03-28 00:20:53 +0000

    Updated time.

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ $updated = \ExchangeApi\Valuations::fetch();
 
 if($updated){
   $data = \ExchangeApi\Valuations::get_data();
-  $time_update = date("Y-m-d H:i:s");
+  $time_updated = date("Y-m-d H:i:s");
   foreach($data as $source_name => $source_data){
     foreach($source_data as $from_key => $to){
       foreach($to as $to_key => $data){
