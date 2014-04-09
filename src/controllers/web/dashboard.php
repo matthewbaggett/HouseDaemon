87e70c514ddb5b4310ffbc372fdd0331cc0a33ad
Matthew Baggett <matthew@baggett.me>
2014-04-09 17:52:36 +0100

    Remove stupid count from dashboard, align rename buttons

--- src/controllers/web/dashboard.php
+++ src/controllers/web/dashboard.php
@@ -5,6 +5,5 @@ $app->get('/dashboard', function () use ($app) {
 
   $app->render('dashboard/dashboard.phtml', array(
     'accounts' => \LoneSatoshi\Models\Account::search()->where('user_id', $_SESSION['user']->user_id)->exec(),
-    'block_count' => \LoneSatoshi\Models\Setting::get("block_count")
   ));
 });
